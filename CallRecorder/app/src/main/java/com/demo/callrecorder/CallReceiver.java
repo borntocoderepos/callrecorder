4e81b9df152c906fb869596914b202dc4dacf9a0
Syed Bilal <syedbilal228@gmail.com>
2021-02-17 18:29:03 +0100

    call recoreder initial commit

---
+++ CallRecorder/app/src/main/java/com/demo/callrecorder/CallReceiver.java
@@ -0,0 +1,29 @@
+package com.demo.callrecorder;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.view.Gravity;
+import android.widget.Toast;
+
+public class CallReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){
+            showToast(context,"Call started...");
+        }
+        else if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_IDLE)){
+            showToast(context,"Call ended...");
+        }
+        else if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)){
+            showToast(context,"Incoming call...");
+        }
+    }
+
+    void showToast(Context context,String message){
+        Toast toast=Toast.makeText(context,message,Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+    }
+}
